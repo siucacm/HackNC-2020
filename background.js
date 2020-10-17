d12dc6761faff418e4b8e4f91f51f9bb19c142f0
cruizk <cruiz.kusman@gmail.com>
2020-10-17 15:45:30 -0500

    Context menu now works when popup is not open

--- background.js
+++ background.js
@@ -7,15 +7,29 @@ chrome.runtime.onInstalled.addListener(function() {
 
 
 function addLink(info,tab) {
-    console.log("Word " + info.selectionText + " was clicked.");
-    chrome.runtime.sendMessage({
-        msg: "add_link",
-        data: { link: info.selectionText }
-    })
+    var views = chrome.extension.getViews({ type: "popup" });
+    if(views.length > 0) {
+        console.log(views);
+        console.log("Word " + info.selectionText + " was clicked.");
+        chrome.runtime.sendMessage({
+            msg: "add_link",
+            data: { link: info.selectionText }
+        })
+    }
+    else {
+        chrome.storage.sync.get('links', function(result) {
+            let links = result.links;
+            links.push(info.selectionText);
+            chrome.storage.sync.set({links}, function() {
+                console.log("Updated Links");
+            })
+        })
+    }
+    
   }
 chrome.contextMenus.create({
-title: "Add To OMM: %s", 
-contexts:["selection"], 
-onclick: addLink
+    title: "Add To OMM: %s", 
+    contexts:["selection"], 
+    onclick: addLink
 });
 
