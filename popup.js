ad2681dd50e2b87a37d26cbd0993b8d98e457a07
cruizk <cruiz.kusman@gmail.com>
2020-10-17 14:42:28 -0500

    Delete functionality is now a go, can no longer spam empty strongs

--- popup.js
+++ popup.js
@@ -1,17 +1,49 @@
-function makeList(links) {
-    const list = document.getElementById("link-list")
-    links.forEach((val, index) => {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(val))
-        list.appendChild(li)
+const list = document.getElementById("link-list")
+
+let linkData = [];
+
+chrome.storage.sync.get('links', function(result) {
+    console.log(result);
+    linkData = result.links;
+    makeList()
+})
+
+
+function makeList() {
+    linkData.forEach((val, index) => {
+        appendList(val, index)
     })
 }
 
-function appendList(link) {
-    const list = document.getElementById('link-list');
+function deleteFromList(link, index) {
+    linkData.splice(index, 1)
+    console.log(index);
+    link.remove();
+    chrome.storage.sync.set({links: linkData}, function() {
+        console.log("DELETED", linkData);
+    })
+}
+
+function appendList(link, index) {
     var li = document.createElement('li')
-    li.appendChild(document.createTextNode(link));
+    var a = document.createElement('a');
+    var s = document.createElement('span')
+    a.setAttribute('href', link);
+    a.setAttribute('target', '_blank');
+    a.classList.add('list-item-link');
+    a.appendChild(document.createTextNode(link));
+    
+    s.appendChild(document.createTextNode('DELETE'));
+    s.classList.add('list-item-delete');
+    s.addEventListener('click', function() { deleteFromList(li, index)});
+
+
+    li.classList.add('list-item')
+    li.appendChild(a);
+    li.appendChild(s);
+    
     list.appendChild(li);
+    
 }
 
 const form = document.getElementById("form");
@@ -21,17 +53,12 @@ console.log(form);
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     const link = document.getElementById('text').value
-    chrome.storage.sync.get('links', function(result) {
-        let links = result.links
-        links.push(link);
-        console.log(links)
-        chrome.storage.sync.set({links}, function() {
-            appendList(link)
-            document.getElementById('text').value = '';
-        })
+    if(!link) return;
+    linkData.push(link);
+    chrome.storage.sync.set({links:linkData}, function() {
+        appendList(link, linkData.length-1);
+        document.getElementById('text').value = '';
     })
 })
 
-chrome.storage.sync.get('links', function(result) {
-    makeList(result.links)
-})
+
